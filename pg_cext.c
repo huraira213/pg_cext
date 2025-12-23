17c46dcb80d98c240ed94e40e0bd7537af25c2de
huraira <hurairakhurshid4@gmail.com>
2025-12-23 09:44:21 +0500

    Stage 1: Basic PostgreSQL C extension with arithmetic functions

--- pg_cext.c
+++ pg_cext.c
@@ -4,6 +4,9 @@
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(add_nums);
+PG_FUNCTION_INFO_V1(mul_nums);
+PG_FUNCTION_INFO_V1(sub_nums);
+PG_FUNCTION_INFO_V1(divide_nums);
 
 Datum add_nums(PG_FUNCTION_ARGS)
 {
@@ -11,4 +14,33 @@ Datum add_nums(PG_FUNCTION_ARGS)
     int32 arg2 = PG_GETARG_INT32(1);
 
     PG_RETURN_INT32(arg1 + arg2);
+}
+
+Datum mul_nums(PG_FUNCTION_ARGS)
+{
+    int32 arg1 = PG_GETARG_INT32(0);
+    int32 arg2 = PG_GETARG_INT32(1);
+
+    PG_RETURN_INT32(arg1 * arg2);
+}
+
+Datum sub_nums(PG_FUNCTION_ARGS)
+{
+    int32 arg1 = PG_GETARG_INT32(0);
+    int32 arg2 = PG_GETARG_INT32(1);
+
+    PG_RETURN_INT32(arg1 - arg2);
+}
+
+Datum divide_nums(PG_FUNCTION_ARGS)
+{
+    int32 arg1 = PG_GETARG_INT32(0);
+    int32 arg2 = PG_GETARG_INT32(1);
+
+    if (arg2 == 0)
+        ereport(ERROR,
+                (errcode(ERRCODE_DIVISION_BY_ZERO),
+                 errmsg("division by zero")));
+
+    PG_RETURN_INT32(arg1 / arg2);
 }
